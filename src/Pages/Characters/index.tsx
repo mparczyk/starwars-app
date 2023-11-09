fc95fb78c0e3bec18fb2ba2ac6010a52610798a2
mparczyk <marekparczyk@gmail.com>
2023-11-09 15:00:14 +0100

    setting person id

--- src/Pages/Characters/index.tsx
+++ src/Pages/Characters/index.tsx
@@ -1,6 +1,7 @@
 import { usePersonQuery } from "../../queries/queries";
 import { List, Card } from "antd";
 import { IPerson } from "../../types";
+import { Link } from "react-router-dom";
 
 export const CharacterPage = (): JSX.Element => {
   const { data: characters } = usePersonQuery();
@@ -9,13 +10,18 @@ export const CharacterPage = (): JSX.Element => {
     <List
       grid={{ gutter: 16, column: 4 }}
       dataSource={characters?.results}
-      renderItem={(person: IPerson) => (
-        <List.Item>
-          <Card title={person.name}>
-            <p>{person.gender}</p>
-          </Card>
-        </List.Item>
-      )}
+      renderItem={(person: IPerson) => {
+        const id = person.url.split("/");
+        return (
+          <List.Item>
+            <Card
+              title={<Link to={`/characters/${id[5]}`}>{person.name}</Link>}
+            >
+              <p>{person.gender}</p>
+            </Card>
+          </List.Item>
+        );
+      }}
     />
   );
 };
